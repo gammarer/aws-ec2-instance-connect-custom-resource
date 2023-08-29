60312773948db267cb190386b27909b78c558bc9
yicr <yicr@users.noreply.github.com>
2023-08-29 10:46:25 +0900

    fix: typescript version 4.9.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
-  typescriptVersion: '5.0.4',
+  typescriptVersion: '4.9.x',
   jsiiVersion: '~5.0.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-ec2-instance-connect-custom-resource',
